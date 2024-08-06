eda1c179ec0ebaf60b19e31634d1d119e2448413
Margarita <margarita.levitm@checkmarx.com>
2024-08-06 13:48:21 +0300

    Update Update SQLInjectionController.java

--- Update SQLInjectionController.java
+++ Update SQLInjectionController.java
@@ -48,7 +48,7 @@ public class SQLInjectionController extends AbstractController {
     }
 
     private List<User> selectUsers(String name, String password) 
-		//* Start SQLi Section
+		
 		String sql = "SELECT  name, secret from USERS where name='"+ name + "' or password='"+ password + "'" ;
 		return jdbcTemplate.query(sql, new RowMapper<User>() {
                     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -58,9 +58,7 @@ public class SQLInjectionController extends AbstractController {
                         return user;
                     }
 				});
-		/*/ End SQLi Section
-            
-		/* Start Remediated Section
+		
 		String sql = "SELECT name, secret from USERS where name=? or password=?";
 		return jdbcTemplate.query(sql, new PreparedStatementCallback<Boolean>(){
 			@Override  
@@ -80,7 +78,7 @@ public class SQLInjectionController extends AbstractController {
 				return user;
 			}
 		});
-		//*/ End Remediated Section
+		
     }
 }
 Footer
